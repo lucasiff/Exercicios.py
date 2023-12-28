4da5895da75ce3cfe5e924b15ef09c21e9feb41a
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-12-28 13:53:20 -0300

    Delete Exercicio_7.py

--- Exercicio_7.py
+++
@@ -1,4 +0,0 @@
-camisas=int(input("Entre com o total de camisas:"))
-calcas=int(input("Entre com o total de calças:"))
-meias=int(input("Entre com o total de meias: "))
-print("O total gasto é =",camisas*5+calcas*10+meias*2)
