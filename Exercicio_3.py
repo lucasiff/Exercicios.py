04bbf9c7c6190586cf1e5fed10254954b9b45f70
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-12-28 13:52:34 -0300

    Delete Exercicio_3.py

--- Exercicio_3.py
+++
@@ -1,4 +0,0 @@
-comprimento=int(input("Digite o comprimento da caixa:"))
-altura=int(input("Digite a altura da caixa:"))
-largura=int(input("Digite a largura da caixa:"))
-print("O volume da caixa é = ",comprimento*altura*largura)
