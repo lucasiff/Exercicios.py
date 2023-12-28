3be3920b1ca5d4ff04f1b7005710e2852f4940c3
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-12-28 13:52:57 -0300

    Delete Exercicio_5.py

--- Exercicio_5.py
+++
@@ -1,4 +0,0 @@
-So=int(input("Escreva o valor do espaço inicial: "))
-V=int(input("Escreva o valor da velocidade:"))
-T=int(input("Escreva o valor do tempo:"))
-print("O valor do espaço percorrido é = ",So+V*T)
