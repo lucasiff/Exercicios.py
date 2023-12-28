213a27eb9a06a3f3447e49a9d935d07ea4d8d31a
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-12-28 13:53:35 -0300

    Delete Exercicio_8.py

--- Exercicio_8.py
+++
@@ -1,8 +0,0 @@
-valor_1=float(input("Digite a primeira nota:"))
-peso_1=float(input("Digite o peso da primeira nota:"))
-valor_2=float(input("Digite a segunda nota:"))
-peso_2=float(input("Digite o peso da segunda nota:"))
-valor_3=float(input("Digite a terceira nota:"))
-peso_3=float(input("Digite o peso da terceira nota:"))
-print("O resultado final é = ",((valor_1*peso_1)+(valor_2*peso_2)+(valor_3*peso_3))/peso_1+peso_2+peso_3)
-
