cbf859bcbbfdeb71df884d7fcc4dc1b246ddb527
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-12-30 08:01:27 -0300

    Delete Estruturas Sequenciais em python/Exercicio_11.py

--- Estruturas Sequenciais em python/Exercicio_11.py
+++
@@ -1,4 +0,0 @@
-salario_mensal=float(input("Digite o seu salario mensal:"))
-aumento=float(input("Digite a porcentagem de aumento:"))
-reajuste=salario_mensal*aumento/100
-print("O seu novo salario é = ",reajuste+salario_mensal)
